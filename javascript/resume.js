23aa22334ce85e3d6c01583e4111971b13427c7a
Florence Schwartz <schflo@gmail.com>
2018-04-15 12:42:13 +1000

    created a resume using javascript and jquery

---
+++ javascript/resume.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+  var slideContainer = $('.slick-containers');
+
+  slideContainer.slick();
+
+  // $('.card').hide();
+  // $('.slick-active').find('.card').fadeIn(200);
+
+  // // On before slide change
+  // slideContainer.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+  //   $('.slick-active').find('.clash-card img').fadeOut(1000);
+  // });
+
+  // // On after slide change
+  // slideContainer.on('afterChange', function(event, slick, currentSlide) {
+  //   $('.slick-active').find('.clash-card img').fadeIn(200);
+  // });
+
+});
